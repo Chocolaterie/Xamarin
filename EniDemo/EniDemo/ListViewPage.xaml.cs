7669580c7255b12befe1c23dd5b900345470cbf8
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2022-10-11 17:09:33 +0200

    Correction TP 2 et ListView HUGO

---
+++ EniDemo/EniDemo/ListViewPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using EniDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EniDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewPage : ContentPage
+    {
+        ObservableCollection<Person> PersonList;
+
+        public ListViewPage()
+        {
+            InitializeComponent();
+
+            PersonList = new ObservableCollection<Person>();
+
+            PersonList.Add(new Person { Firstname = "Isaac" });
+            PersonList.Add(new Person { Firstname = "Hugo" });
+            PersonList.Add(new Person { Firstname = "Test" });
+
+            this.personView.ItemsSource = PersonList;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            PersonList[1].Firstname = "CHOCOLATINE";
+            PersonList[1] = PersonList[1];
+        }
+    }
+}
---
+++ EniDemo/EniDemo/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EniDemo.Models
+{
+    public class Person
+    {
+        public String Firstname {get; set;}
+    }
+}
